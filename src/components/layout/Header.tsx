fa3fea9d75b0b430b429e391dc57647882f01f17
dini28 <dipeshsonitech@gmail.com>
2025-12-20 18:52:29 +0530

    change the pdf location

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
-import CV from '../../../public/CV.pdf';
+import CV from '../../assets/CV.pdf';
 
 const navLinks = [
     { href: "#about", label: "About" },
